94994168f762ae5f0f3ce47a66c146a867ce9f96
khanhvu4603 <khanhvu4603@gmail.com>
2025-12-08 13:18:23 +0700

    Fix critical port issue for Railway

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors(); // Enable CORS for Dashboard and Client
   const port = process.env.PORT || 4000;
-  await app.listen(4000);
+  await app.listen(port);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
